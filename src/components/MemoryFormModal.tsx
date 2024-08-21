821acdcfb8194e97cf54ef00d4a9d4ac9dbb5d45
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-08-20 20:03:32 -0400

    added close button to the top right corner

--- src/components/MemoryFormModal.tsx
+++ src/components/MemoryFormModal.tsx
@@ -1,6 +1,13 @@
 import { useEffect } from 'react'
 import { useForm, Controller, SubmitHandler } from 'react-hook-form'
-import { Modal, FormControl, TextField, Button } from '@mui/material'
+import {
+  Modal,
+  FormControl,
+  TextField,
+  Button,
+  IconButton,
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 
 import type { MemoryType } from '../types/app'
 
@@ -76,8 +83,15 @@ export default function MemoryFormModal(props: MemoryFormModalProps) {
       aria-describedby='Memory Form Modal'
     >
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-lg'>
-        <form onSubmit={handleSubmit(onSubmit)} className='p-2 w-96 bg-white'>
-          <h2 className='text-2xl font-semibold mb-6'>
+        <IconButton
+          aria-label='close'
+          onClick={handleClose}
+          style={{ position: 'absolute', top: 20, right: 20 }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <form onSubmit={handleSubmit(onSubmit)} className='p-2 w-120 bg-white'>
+          <h2 className='text-3xl font-semibold mb-3'>
             {isNewMemory ? 'Add' : 'Edit'} Memory
           </h2>
           <Controller
@@ -146,7 +160,7 @@ export default function MemoryFormModal(props: MemoryFormModalProps) {
               </FormControl>
             )}
           />
-          <div className='flex justify-end'>
+          <div className='flex justify-end mt-4'>
             <Button
               variant='outlined'
               color='primary'

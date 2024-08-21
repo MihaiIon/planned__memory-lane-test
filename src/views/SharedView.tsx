80711b0032923800da6e78e6d7612c6a64f1c347
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-08-21 14:36:45 -0400

    added shared button the shared view

--- src/views/SharedView.tsx
+++ src/views/SharedView.tsx
@@ -1,4 +1,5 @@
 import MemoryList from '../components/MemoryList'
+import ShareButton from '../components/ShareButton'
 
 import logoImg from '../assets/logo.png'
 
@@ -21,11 +22,12 @@ export default function SharedView() {
   }
 
   return (
-    <main className='h-screen w-full bg-gray-50 pt-28 pb-20 px-2 sm:px-8'>
+    <main className='min-h-screen w-full bg-gray-50 pt-28 pb-32 px-2 sm:px-8'>
       <img src={logoImg} alt='Memory Lane Logo' className='mx-auto w-32 mb-8' />
 
       <h1 className='text-4xl font-medium mt-4 text-center'>
         <span>{fakeUser.firstName}'s Memory Lane</span>
+        <ShareButton />
       </h1>
 
       <hr className='my-4 w-36 mx-auto border-t-1 border-gray-200 my-6' />
